80908ed00a2b87a9aa1d0aa8569c7fab7ac7cb2f
Arley.Gomez <arley.gomez@globalmvm.com>
2026-04-13 09:16:50 -0500

    Clase 3 - Control props Parent - Child

--- src/components/ControlProps/ControlProps.tsx
+++ src/components/ControlProps/ControlProps.tsx
@@ -1,6 +1,36 @@
+import React from "react";
+
+type TooggleProps = {
+  isToggled?: boolean;
+  onToggle?: (value: boolean) => void;
+};
+
+export const Toggle: React.FC<TooggleProps> = ({ isToggled = false, onToggle }) => {
+  const [internalToggle, setInternalToggle] = React.useState(isToggled);
+
+  const toggle = () => {
+    const newToggle = !internalToggle;
+    setInternalToggle(newToggle);
+    
+    if (onToggle) {
+      onToggle(newToggle);
+    }
+  };
+
+  return (
+    <button onClick={toggle}>
+      {internalToggle ? "On 😎" : "Off 😖"}
+    </button>
+  );
+};
+
 export const ParentComponent = () => {
+  const [toggleState, setToggleState] = React.useState(false);
+
   return (
     <div>
+      <p>The toggle is {toggleState ? "On 😎" : "Off 😖"}</p>
+      <Toggle isToggled={toggleState} onToggle={setToggleState} />
     </div>
   );
 };
