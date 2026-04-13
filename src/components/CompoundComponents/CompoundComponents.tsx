be37490981f5cf5732cbea2aa0e558bca8af1c6b
Arley.Gomez <arley.gomez@globalmvm.com>
2026-04-13 15:21:56 -0500

    Clase 4 - Patrón Compound Components

--- src/components/CompoundComponents/CompoundComponents.tsx
+++ src/components/CompoundComponents/CompoundComponents.tsx
@@ -1,6 +1,12 @@
+import Tab from "./Tab";
+import Tabs from "./Tabs";
+
 export const CompoundComponents = () => {
   return (
-    <>
-    </>
+    <Tabs>
+      <Tab label="Girafa">🦒</Tab>
+      <Tab label="Elefante">🐘</Tab>
+      <Tab label="Leon">🦁</Tab>
+    </Tabs>
   );
 };
