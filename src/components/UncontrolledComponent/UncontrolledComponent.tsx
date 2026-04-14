aa8f6c86f168edddaa03640ba6afd6fa2310b15c
Arley.Gomez <arley.gomez@globalmvm.com>
2026-04-14 13:14:26 -0500

    Clase 8 - Componentes Controlados y No Controlados en Formularios

--- src/components/UncontrolledComponent/UncontrolledComponent.tsx
+++ src/components/UncontrolledComponent/UncontrolledComponent.tsx
@@ -1,6 +1,22 @@
+import React from 'react';
+
 export const UncontrolledInput = () => {
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  
+  const handleSubmit = () => {
+    if (inputRef.current) {
+      alert(`Producto añadido al carrito: ${inputRef.current.value} 😅`);
+    }
+  };
+
   return (
     <div>
+      <input 
+        type="text" 
+        placeholder="Nombre del producto. Ej: Manzana"
+        ref={inputRef}
+      />
+      <button onClick={handleSubmit}>Añadir al carrito</button>
     </div>
   );
 };
